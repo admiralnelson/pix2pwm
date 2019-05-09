0b9379c22b82e10db6e844808e690ffefa1005e8
horatio nelson <admiralofinternetmeme@outlook.com>
2019-05-09 15:23:13 +0700

    Add project files.

---
+++ robot.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		robot.ino
+ Created:	5/9/2019 3:22:53 PM
+ Author:	MIIX 510
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
